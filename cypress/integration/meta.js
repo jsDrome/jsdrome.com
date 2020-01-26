20716a96ab9ad91e808f5fabd65ef00f9201e2e8
Sreeram Padmanabhan <sreeram@jsdrome.com>
2020-01-26 22:09:33 +0100

    feat: cypress tests

--- cypress/integration/meta.js
+++ cypress/integration/meta.js
@@ -1,7 +1,4 @@
-/* eslint-disable no-magic-numbers */
-/* eslint-disable no-undef */
-import { STAGE_URL, URL, DESCRIPTION } from '../../src/variables';
-import links from '../../src/client/list';
+import { STAGE_URL} from '../../src/variables';
 
 beforeEach(() => {
   if (window.navigator && navigator.serviceWorker) {
@@ -14,15 +11,16 @@ beforeEach(() => {
   }
 })
 
-describe('My First Test', function () {
-  it('Visits the Home page', function () {
-    const post = links[0].links[0];
+describe('Cypress tests', function () {
+  it('Checks title', function () {
     cy.visit(STAGE_URL);
-    cy.title().should('eq', post.title + ' | ' + DESCRIPTION);
-    cy.get('meta[name="description"]').should("have.attr", "content", post.description);
-    cy.get('meta[property="og:description"]').should("have.attr", "content", post.ogDescription);
-    cy.get('meta[property="og:title"]').should("have.attr", "content", post.ogTitle);
-    cy.get('meta[property="og:url"]').should("have.attr", "content", URL + '/');
-    cy.get('meta[property="og:image"]').should("have.attr", "content", post.ogImage);
-  })
+    cy.title().should('eq', 'jsDrome | Mentor driven open source transformation');
+  });
+  it('Checks payment', function () {
+    cy.visit(STAGE_URL);
+    cy.wait(2000);
+    cy.get('#paytm-payment-btn').click();
+    cy.contains('jsDrome Order');
+    cy.get('.backbtn button').click();
+  });
 });
